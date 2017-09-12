655973938c3161b88449629c27377edd930459f5
Muhammad H.Khan <khan.15@wright.edu>
2017-09-12 19:49:22 -0400

    modified hello a & b

--- hello4110-b.java
+++ hello4110-b.java
@@ -1,6 +1,6 @@
-class Hello4110{
+class Hello4110b{
 	public static void main(String[] args){
 		System.out.println("Hello 4110!");
-		System.out.println("original java file");
+		System.out.println("I am in Hello B");
 	}
 }
